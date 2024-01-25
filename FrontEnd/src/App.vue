a97be2da88af6dd0cd68176b45ea36e2513c4935
CosimoA <cosimo.archip@gmail.com>
2024-01-25 18:29:18 +0100

    FEAT: api request work

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,5 +1,20 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      todoArray: [],
+    }
+  },
+  mounted() {
+    axios.get("http://localhost/todoApi.php").then((res) => {
+      this.todoArray = res.data;
+      console.log("Mie API: ", this.todoArray);
+    });
+  },
+};
 </script>
 
 <template></template>
