0882a734aa3740106fb492f3ee1eb8750679dfc6
CosimoA <cosimo.archip@gmail.com>
2024-01-26 10:17:54 +0100

    FEAT: display tasks & push new post

--- BackEnd/todoApi.php
+++ BackEnd/todoApi.php
@@ -1,27 +1,34 @@
 <?php
 
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Headers: X-Requested-With');
+// header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 
-$todoList = array(
-    array(
+$todoList = [
+    [
         'id' => 1,
         'title' => 'Catturare Pikachu',
         'description' => 'Trova Pikachu e catturalo con una Poké Ball.',
         'fatto' => false
-    ),
-    array(
+    ],
+    [
         'id' => 2,
         'title' => 'Sconfiggere il Team Rocket',
         'description' => 'Affronta il Team Rocket e sconfiggili per proteggere la città.',
         'fatto' => true
-    ),
-    array(
+    ],
+    [
         'id' => 3,
         'title' => 'Evolvere il tuo Pokémon',
         'description' => 'Porta il tuo Pokémon in una Palestra per farlo evolvere e potenziarlo.',
         'fatto' => false
-    )
-);
+    ]
 
-echo json_encode($todoList);
+
+];
+
+$todoListJson = json_encode($todoList);
+
+echo $todoListJson;
--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,22 +1,46 @@
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import axios from "axios";
+import axios from 'axios';
 
 export default {
   data() {
     return {
       todoArray: [],
-    }
+      newTask: '',
+    };
   },
   mounted() {
-    axios.get("http://localhost/todoApi.php").then((res) => {
-      this.todoArray = res.data;
-      console.log("Mie API: ", this.todoArray);
-    });
+    axios.get('http://localhost/todoApi.php')
+      .then(res => {
+        this.todoArray = res.data;
+        console.log(this.todoArray);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
   },
-};
+  methods: {
+    pushNewTask() {
+      console.log("hello: " + this.newTask);
+
+    }
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <div>
+    <h1>Todo App: {{ todoArray.length }}</h1>
+    <form @submit.prevent="pushNewTask">
+      <input v-model="newTask" placeholder="Nuovo Task" />
+      <button type="submit">Aggiungi</button>
+    </form>
+    <ul>
+      <li v-for="task in todoArray" :key="task.id">
+        <input type="checkbox" />
+        {{ task.title }}
+      </li>
+    </ul>
+  </div>
+</template>
 
 <style lang="scss" scoped></style>
